430e3c5d6b9ccd29877b6dea01f58af8f97e3fba
Erik Luebbers <erluebbers@gmail.com>
2022-07-25 15:15:17 -0700

    add search button to search form

--- client/src/features/search/SearchContainer.js
+++ client/src/features/search/SearchContainer.js
@@ -1,10 +1,28 @@
+import { useState } from 'react';
 import './Search.css';
 
 
 function SearchContainer() {
+  const [authorName, setAuthorName] = useState({})
+
+  const handleSubmit = () => {
+    console.log()
+  }
   
   return (
     <div>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor='author'>Search by author name</label>
+        <input
+          type="text"
+          id="author"
+          placeholder="Enter Author Name.."
+          value={authorName}
+          onChange={(e) => setAuthorName(e.target.value)}
+        >
+        </input>
+        <button className='button' type="submit">Search</button>
+      </form>
     </div>
   );
 }
